66b6941ded1f6772f71cef9d16c75df06fd8bfe4
mehmetatas <mehmetatasm@gmail.com>
2026-01-29 17:27:24 +1100

    Use Nunito font in networthmonitor

--- networthmonitor/web/src/ui/www/client/layout/AppHtml.tsx
+++ networthmonitor/web/src/ui/www/client/layout/AppHtml.tsx
@@ -36,7 +36,7 @@ export const AppHtml = ({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
 
--- networthmonitor/web/src/ui/www/client/layout/Html.tsx
+++ networthmonitor/web/src/ui/www/client/layout/Html.tsx
@@ -49,7 +49,7 @@ export const Html = ({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
 
